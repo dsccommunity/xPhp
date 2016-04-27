4aa534b7bf1275f65f98be234823ea4c963d5476
Katie Keim <kakeim@microsoft.com>
2016-04-27 14:16:25 -0700

    Removing empty functions and fixing formatting

--- tests/xPhp.Schema.Tests.ps1
+++ tests/xPhp.Schema.Tests.ps1
@@ -1,62 +1,34 @@
-﻿<# 
-.summary
+<# 
+    .SUMMARY
     Test suite for ExportToHtml.psm1
 #>
 [CmdletBinding()]
-param()
+param ()
 
 $xPhpModuleRoot = "${env:ProgramFiles}\WindowsPowerShell\Modules\xPhp"
 
-if(!(test-path $xPhpModuleRoot))
+if (!(Test-Path $xPhpModuleRoot))
 {
     md $xPhpModuleRoot > $null
 }
-Copy-Item -Recurse  $PSScriptRoot\..\* $xPhpModuleRoot -force -exclude '.git'
+Copy-Item -Recurse  $PSScriptRoot\..\* $xPhpModuleRoot -Force -Exclude '.git'
 
 $ErrorActionPreference = 'stop'
 Set-StrictMode -Version latest
 
-function Suite.BeforeAll {
-    # Remove any leftovers from previous test runs
-    Suite.AfterAll 
-
-} 
-
-function Suite.AfterAll {
-}
-
-function Suite.BeforeEach {
-}
-
-try
+Describe 'xPhpProvision' 
 {
-    Describe 'xPhpProvision' {
-        BeforeEach {
-            Suite.BeforeEach
-        }
-
-        AfterEach {
-        }
-
-            It 'Should import without error' {
-                {
-                import-module "$xPhpModuleRoot\DscResources\xPhpProvision\xPhpProvision.Schema.psm1"
-                }|  should not throw
-            }
-
-            It 'Should return from Get-DscResource' {
-                $xphp = Get-DscResource -Name xPhpProvision
-                $xphp.ResourceType | should be 'xPhpProvision'
-                $xphp.Module | should be 'xPhp'
-                $xphp.FriendlyName | should BeNullOrEmpty
-                $xphp.ImplementedAs | should be 'Composite'
-            }
+    It 'Should import without error' 
+    {
+        { Import-Module "$xPhpModuleRoot\DscResources\xPhpProvision\xPhpProvision.Schema.psm1" } | Should Not throw
     }
 
-
+    It 'Should return from Get-DscResource' 
+    {
+        $xphp = Get-DscResource -Name xPhpProvision
+        $xphp.ResourceType | Should Be 'xPhpProvision'
+        $xphp.Module | Should Be 'xPhp'
+        $xphp.FriendlyName | Should BeNullOrEmpty
+        $xphp.ImplementedAs | Should Be 'Composite'
+    }
 }
-finally
-{
-    Suite.AfterAll
-}
-
